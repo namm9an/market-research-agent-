13698eda534c83a433e37f4d313de01080114246
namm9an <naman.moudgill77@gmail.com>
2026-02-25 20:18:29 +0530

    feat(frontend): implement Matomo tracking

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import Sidebar from "@/components/Sidebar";
 import "./globals.css";
 
@@ -27,6 +28,23 @@ export default function RootLayout({
         <div className="flex-1 overflow-y-auto relative">
           {children}
         </div>
+
+        {/* Matomo Analytics (Internal Sales Tracker) */}
+        <Script id="matomo-tracking" strategy="afterInteractive">
+          {`
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//localhost:8080/matomo/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
